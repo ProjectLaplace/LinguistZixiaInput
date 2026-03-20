ac27ab7bbae18877a2f0894ef1e6c5f0b28dd10e
Rainux Luo <rainux@gmail.com>
2026-03-20 23:45:41 +0800

    Create blank IME app

---
+++ Apps/LaplaceIME/LaplaceIMETests/LaplaceIMETests.swift
@@ -0,0 +1,17 @@
+//
+//  LaplaceIMETests.swift
+//  LaplaceIMETests
+//
+//  Created by Rainux Luo on 2026/3/20.
+//
+
+import Testing
+@testable import LaplaceIME
+
+struct LaplaceIMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
